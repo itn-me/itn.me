dece1e713f0507170476824146d6ae9333c333bb
Aleksandar Scepanovic <aco@itn.me>
2019-05-16 16:17:56 +0200

    New assets and base for the site added

---
+++ assets/js/contactme-config.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * ContactMe
+ * https://www.itn.me
+ *
+ */
+
+// Language options
+var cm_lang_path = 'assets/lang/en.json';			            // Choose the language of the error messages
+var cm_datepicker_lang = 'en';							// Choose the language of the date picker plugin
+var cm_dropdown_lang = 'en';							// Choose the language of the dropdown plugin
